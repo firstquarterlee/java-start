a706dde611f14d24037de81e2f5e41ed702f8dd9
firstquarterlee <firstquarterlee@gmail.com>
2023-12-13 00:23:52 +0900

    논리 연산자, 대입 연산자

---
+++ src/operator/Logical1.java
@@ -0,0 +1,8 @@
+package operator;
+
+public class Logical1 {
+    public static void main(String[] args) {
+        System.out.println("&&: AND 연산");
+        System.out.println(true && true);
+    }
+}
