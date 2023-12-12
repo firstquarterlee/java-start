3f97f433785d9ecc1a59a20ba1e38eeaf11d7186
firstquarterlee <firstquarterlee@gmail.com>
2023-12-13 00:01:43 +0900

    산술 연산자, 문자열 더하기, 연산자 우선순위, 증감 연산자, 비교 연산자

---
+++ src/operator/Operator2.java
@@ -0,0 +1,9 @@
+package operator;
+
+public class Operator2 {
+    public static void main(String[] args) {
+        //문자열과 문자열 더하기1
+        String result1 = "hello " + "world";
+        System.out.println(result1);
+    }
+}
